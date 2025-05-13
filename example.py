9eeeff86a68cf560e779f7588557b9caf2c88023
anamoyee <thecreatorrr333@gmail.com>
2025-05-13 21:29:29 +0200

    Populate README.md and add an example

--- example.py
+++ example.py
@@ -1,10 +1,5 @@
-# str2td
-
-Easily express a timedelta in a succinct and human-readable way.
-
-## Usage example
-```python
 from datetime import datetime
+
 from lark import LarkError
 from str2td import str2td
 
@@ -30,35 +25,3 @@ except LarkError as e:
 	print("=" * 50)
 	print(e)
 	print("=" * 50)
-
-```
-<details>
-<summary>Successful inputs</summary>
-
-```
-now=2025-05-13 21:01:41 (Central European Summer Time)
-
-Set a reminder in: 3d!10:
-Setting reminder at 2025-05-17 10:00:00 (3 days, 12:58:18.493712 from now).
-
-When's your birthday? 10-10
-Great! Your nearest birthday is at: 2025-10-10 21:01:41, in 150 days.
-
-[Seconds calculator]: 3d-.25d5.5h1s
-3d-.25d5.5h1s = 257401.0 seconds.
-```
-</details>
-
-<details>
-<summary>Erroring inputs</summary>
-
-```
-now=2025-05-13 21:12:22 (Central European Summer Time)
-
-Set a reminder in: 1huj
-Invalid Syntax!
-Error trying to process rule "robostr_segment":
-
-Unknown unit: 'huj'
-```
-</details>
