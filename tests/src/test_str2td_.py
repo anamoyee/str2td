3c9c55da54a27965cd2a187f4cbd095b6f73b0d2
anamoyee <thecreatorrr333@gmail.com>
2025-05-13 22:43:16 +0200

    add more tests

--- tests/src/test_str2td_.py
+++ tests/src/test_str2td_.py
@@ -6,6 +6,7 @@ from datetime import timedelta as Δ
 from functools import partial, reduce
 
 import pytest
+from lark import LarkError
 from str2td import str2td
 
 tz = Dt.now().astimezone().tzinfo
@@ -132,3 +133,65 @@ def test_weekday(*, dt: Dt, expected_offset: int, case_fn: Callable[[str], str])
 	assert f2("fri") == f2("friday") == Δ(days=(expected_offset + 4) % 7 or 7)
 	assert f2("sat") == f2("saturday") == Δ(days=(expected_offset + 5) % 7 or 7)
 	assert f2("sun") == f2("sunday") == Δ(days=(expected_offset + 6) % 7 or 7)
+
+
+def test_time():
+	now = Dt(2025, 5, 12, 10, 0, 0, tzinfo=tz)
+	f2 = partial(f, now=now)
+
+	assert now + f2("10:") == Dt(2025, 5, 13, 10, 0, 0, tzinfo=tz)
+	assert now + f2("10:00:01") == Dt(2025, 5, 12, 10, 0, 1, tzinfo=tz)
+
+	assert now + f2("9:") == Dt(2025, 5, 13, 9, 0, 0, tzinfo=tz)
+
+
+@pytest.mark.parametrize(
+	("now", "s", "dt"),
+	(
+		(outer_dt, k, v)
+		for outer_dt, dct in (
+			{
+				Dt(2025, 5, 12, 10, 0, 0, tzinfo=tz): {
+					"30-": Dt(2025, 5, 30, 10, 0, 0, tzinfo=tz),
+					"30-5": Dt(2025, 5, 30, 10, 0, 0, tzinfo=tz),
+					"30-5-2004": Dt(2004, 5, 30, 10, 0, 0, tzinfo=tz),
+					"30-5-04": Dt(2004, 5, 30, 10, 0, 0, tzinfo=tz),
+					"30-5-4": Dt(2004, 5, 30, 10, 0, 0, tzinfo=tz),
+				},
+				Dt(2024, 2, 20, 10, 0, 0, tzinfo=tz): {
+					"28-": Dt(2024, 2, 28, 10, 0, 0, tzinfo=tz),
+					"29-": Dt(2024, 2, 29, 10, 0, 0, tzinfo=tz),
+					"30-": Dt(2024, 3, 30, 10, 0, 0, tzinfo=tz),
+				},
+				Dt(2025, 2, 20, 10, 0, 0, tzinfo=tz): {
+					"28-": Dt(2025, 2, 28, 10, 0, 0, tzinfo=tz),
+					"29-": Dt(2025, 3, 29, 10, 0, 0, tzinfo=tz),
+					"30-": Dt(2025, 3, 30, 10, 0, 0, tzinfo=tz),
+				},
+				Dt(2025, 12, 31, 10, 0, 0, tzinfo=tz): {
+					"1-": Dt(2026, 1, 1, 10, 0, 0, tzinfo=tz),
+					"1-4": Dt(2026, 4, 1, 10, 0, 0, tzinfo=tz),
+				},
+			}.items()
+		)
+		for k, v in dct.items()
+	),
+)
+def test_date(*, now: Dt, s: str, dt: Dt):
+	assert now + f(s, now=now) == dt
+
+
+@pytest.mark.parametrize(
+	("s",),
+	(
+		("",),  # unexpected eof
+		("1",),  # expected unit
+		("huj",),  # unexpected unit
+		("10-10-10000",),  # date out of range
+		("10-10:00",),  # unexpected : in -
+		("10:00:00:00",),  # clock has 3 figures at most buh
+	),
+)
+def test_invalid_syntax(*, s: str, now: Dt = Dt(2025, 5, 12, 10, 0, 0, tzinfo=tz)):
+	with pytest.raises(LarkError):
+		f(s, now=now)
